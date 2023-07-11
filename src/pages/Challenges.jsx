c03b2395004185057c0ccf6d4e22cd75415f9070
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-07-11 18:14:35 +0530

    [Add]. Challenges and Vitals Basic Pages

--- src/pages/Challenges.jsx
+++ src/pages/Challenges.jsx
@@ -1,87 +1,254 @@
 import React, { useEffect, useState } from "react";
+import { Card, Button, Modal, Form } from "react-bootstrap";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 
 const Challenges = () => {
 	const [challenges, setChallenges] = useState([]);
-	const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
-    const [cookies] = useCookies(["jwt"]);
-	// Add other necessary state variables for challenge form
-    axios.defaults.headers.common["Authorization"] = `Bearer ${cookies.jwt}`;
-	useEffect(() => {
-		fetchChallenges();
-	}, []);
-
-	const fetchChallenges = async () => {
+	const [showCreateModal, setShowCreateModal] = useState(false);
+	const [showParticipateModal, setShowParticipateModal] = useState(false);
+	const [selectedChallenge, setSelectedChallenge] = useState(null);
+	const [cookies] = useCookies(["jwt"]);
+	axios.defaults.withCredentials = true;
+	axios.defaults.headers.common["Authorization"] = `Bearer ${cookies.jwt}`;
+	const fetchData = async () => {
 		try {
-			const response = await axios.get("https://localhost:7155/api/Challenges");
+			const response = await axios.get("https://localhost:7155/api/challenges");
 			setChallenges(response.data);
 		} catch (error) {
-			// Handle error
+			console.error(error);
 		}
 	};
 
-	const createChallenge = async (e) => {
-		e.preventDefault();
-		const newChallenge = {
-			name,
-			description,
-			// Add other necessary challenge properties
-		};
+	useEffect(() => {
+		fetchData();
+	}, []);
 
+const handleCreateChallenge = async (event) => {
+	event.preventDefault();
+
+	const form = event.target;
+	const formData = new FormData(form);
+
+	const challenge = {
+		challengeId: "string",
+		name: formData.get("name"),
+		description: formData.get("description"),
+		calories: parseInt(formData.get("calories")) || null,
+		kilometers: parseInt(formData.get("kilometers")) || null,
+		steps: parseInt(formData.get("steps")) || null,
+		startDate: formData.get("startDate"),
+		endDate: formData.get("endDate"),
+		createdBy: "string",
+	};
+
+	try {
+		const response = await axios.post(
+			"https://localhost:7155/api/challenges",
+			challenge
+		);
+		setChallenges([...challenges, response.data]);
+		setShowCreateModal(false);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+
+	const handleParticipateChallenge = async (challengeId) => {
 		try {
-			await axios.post("https://localhost:7155/api/Challenges", newChallenge);
-			setName("");
-			setDescription("");
-			// Clear other form fields
-			fetchChallenges();
+			await axios.post(
+				`https://localhost:7155/api/challenges/${challengeId}/participants`
+			);
+			fetchData();
 		} catch (error) {
-			// Handle error
+			console.error(error);
 		}
 	};
 
-	const deleteChallenge = async (challengeId) => {
+	const handleRemoveParticipant = async (challengeId) => {
 		try {
-			await axios.delete(`https://localhost:7155/api/Challenges/${challengeId}`);
-			fetchChallenges();
+			await axios.delete(
+				`https://localhost:7155/api/challenges/${challengeId}/participants`
+			);
+			fetchData();
 		} catch (error) {
-			// Handle error
+			console.error(error);
 		}
 	};
+
+	const handleDeleteChallenge = async (challengeId) => {
+		try {
+			await axios.delete(
+				`https://localhost:7155/api/challenges/${challengeId}`
+			);
+			setChallenges(
+				challenges.filter((challenge) => challenge.challengeId !== challengeId)
+			);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const renderCreateModal = () => (
+		<Modal show={showCreateModal} onHide={() => setShowCreateModal(false)}>
+			<Modal.Header closeButton>
+				<Modal.Title>Create Challenge</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<form onSubmit={handleCreateChallenge}>
+					<div className="mb-3">
+						<label htmlFor="name">Name</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							className="form-control"
+							required
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="description">Description</label>
+						<input
+							type="text"
+							id="description"
+							name="description"
+							className="form-control"
+							required
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="calories">Calories</label>
+						<input
+							type="number"
+							id="calories"
+							name="calories"
+							className="form-control"
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="kilometers">Kilometers</label>
+						<input
+							type="number"
+							id="kilometers"
+							name="kilometers"
+							className="form-control"
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="steps">Steps</label>
+						<input
+							type="number"
+							id="steps"
+							name="steps"
+							className="form-control"
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="startDate">Start Date</label>
+						<input
+							type="date"
+							id="startDate"
+							name="startDate"
+							className="form-control"
+							required
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="endDate">End Date</label>	
+						<input
+							type="date"
+							id="endDate"
+							name="endDate"
+							className="form-control"
+							required
+						/>
+					</div>
+					<button type="submit" className="btn btn-primary">
+						Create
+					</button>
+				</form>
+			</Modal.Body>
+		</Modal>
+	);
+
+	const renderParticipateModal = () => (
+		<Modal
+			show={showParticipateModal}
+			onHide={() => setShowParticipateModal(false)}
+		>
+			<Modal.Header closeButton>
+				<Modal.Title>Participate in Challenge</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<p>Are you sure you want to participate in this challenge?</p>
+			</Modal.Body>
+			<Modal.Footer>
+				<Button
+					variant="secondary"
+					onClick={() => setShowParticipateModal(false)}
+				>
+					Cancel
+				</Button>
+				<Button
+					variant="primary"
+					onClick={() => handleParticipateChallenge(selectedChallenge)}
+				>
+					Participate
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
+
+	const renderChallengeActions = (rowData) => {
+		const { challengeId } = rowData;
+
+		return (
+			<div className="d-flex">
+				<button
+					className="btn btn-sm btn-primary me-2"
+					onClick={() => handleParticipateChallenge(challengeId)}
+				>
+					Participate
+				</button>
+				<button
+					className="btn btn-sm btn-danger"
+					onClick={() => handleRemoveParticipation(challengeId)}
+				>
+					Remove
+				</button>
+			</div>
+		);
+	};
+
+
+	const renderChallengeDeleteButton = (rowData) => (
+		<Button
+			variant="danger"
+			onClick={() => handleDeleteChallenge(rowData.challengeId)}
+		>
+			Delete
+		</Button>
+	);
 
 	return (
 		<div>
-			<h1>Challenges</h1>
-
-			{/* Render the list of challenges */}
-			{challenges.map((challenge) => (
-				<div key={challenge.challengeId}>
-					<h3>{challenge.name}</h3>
-					<p>{challenge.description}</p>
-					{/* Render other challenge properties */}
-					<button onClick={() => deleteChallenge(challenge.challengeId)}>
-						Delete
-					</button>
-				</div>
-			))}
-
-			{/* Create challenge form */}
-			<form onSubmit={createChallenge}>
-				<input
-					type="text"
-					placeholder="Name"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-				/>
-				<textarea
-					placeholder="Description"
-					value={description}
-					onChange={(e) => setDescription(e.target.value)}
-				/>
-				{/* Add other necessary form fields */}
-				<button type="submit">Create Challenge</button>
-			</form>
+			<Button variant="primary" onClick={() => setShowCreateModal(true)}>
+				Create Challenge
+			</Button>
+			<DataTable value={challenges} paginator rows={5}>
+				<Column field="name" header="Name"></Column>
+				<Column field="description" header="Description"></Column>
+				<Column field="calories" header="Calories"></Column>
+				<Column field="kilometers" header="Kilometers"></Column>
+				<Column field="steps" header="Steps"></Column>
+				<Column header="Actions" body={renderChallengeActions}></Column>
+				<Column header="Delete" body={renderChallengeDeleteButton}></Column>
+			</DataTable>
+			{renderCreateModal()}
+			{renderParticipateModal()}
 		</div>
 	);
 };
--- src/pages/Vitals.jsx
+++ src/pages/Vitals.jsx
@@ -1,48 +1,93 @@
 import React, { useState } from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Card } from "react-bootstrap";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 
 const Vitals = () => {
-    const [vitalsData, setVitalsData] = useState(null);
+	const [steps, setSteps] = useState(null);
+	const [distance, setDistance] = useState(null);
+    const [calories, setCalories] = useState(null);
     const [cookies] = useCookies(["jwt"]);
 
-	const fetchData = async () => {
-		try {
-			const response = await axios.get(
-                "https://localhost:7155/api/PersonalVitals/fetch-data", {
-                    headers: {
-                        Authorization: `Bearer ${cookies.jwt}`,
-                    },
-                }
-			);
-			const data = response.data;
-			setVitalsData(data);
-		} catch (error) {
-			// Handle error
-			console.log(error);
+	const fetchData = async (endpoint) => {
+        try {
+            axios.defaults.withCredentials = true;
+            axios.defaults.headers.common["Authorization"] = `Bearer ${cookies.jwt}`;
+            const response = await axios.post(endpoint, {
+                headers: {
+                    Authorization: `Bearer ${cookies.jwt}`,
+                },
+                withCredentials: true,
+            });
+			return response.data;
+    } 
+        catch (error) {
+			console.error(error);
 		}
 	};
 
-	const displayData = () => {
-		if (vitalsData) {
-			// Display the fetched data
-			console.log(vitalsData);
-		} else {
-			// No data available
-			console.log("No data available");
-		}
+	const handleFetchData = async () => {
+		const now = new Date();
+		const startTime = new Date(
+			now.getFullYear(),
+			now.getMonth(),
+			now.getDate(),
+			0,
+			0,
+			0,
+			0
+		).toISOString();
+		const endTime = now.toISOString();
+
+		const stepsData = await fetchData(
+			`https://localhost:7155/api/googlefit/steps?startTime=${startTime}&endTime=${endTime}`
+		);
+		const distanceData = await fetchData(
+			`https://localhost:7155/api/googlefit/distance?startTime=${startTime}&endTime=${endTime}`
+		);
+		const caloriesData = await fetchData(
+			`https://localhost:7155/api/googlefit/calories?startTime=${startTime}&endTime=${endTime}`
+		);
+
+		setSteps(stepsData);
+		setDistance(distanceData);
+		setCalories(caloriesData);
 	};
+
 
 	return (
 		<Container>
-			<h1>Vitals Page</h1>
-			<Button variant="primary" onClick={fetchData}>
+			<h1>Vitals</h1>
+			<Button variant="primary" onClick={handleFetchData}>
 				Fetch Data
 			</Button>
-			<Button variant="secondary" onClick={displayData}>
-				Display Data
-			</Button>
+
+			{steps && (
+				<Card>
+					<Card.Body>
+						<Card.Title>Steps</Card.Title>
+						<Card.Text>{steps}</Card.Text>
+					</Card.Body>
+				</Card>
+			)}
+
+			{distance && (
+				<Card>
+					<Card.Body>
+						<Card.Title>Distance</Card.Title>
+						<Card.Text>{distance}</Card.Text>
+					</Card.Body>
+				</Card>
+			)}
+
+			{calories && (
+				<Card>
+					<Card.Body>
+						<Card.Title>Calories</Card.Title>
+						<Card.Text>{calories}</Card.Text>
+					</Card.Body>
+				</Card>
+			)}
 		</Container>
 	);
 };
