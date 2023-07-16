fc8d63a14f43b96e3af35346c665bbd6ba15ad34
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-07-16 23:27:13 +0530

    [Fix] navbar conditional links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,21 @@ import { Navbar, Nav } from "react-bootstrap";
 const AppNavbar = () => {
 	// const username = localStorage.getItem("userName");
 	const [username, setUsername] = useState('');
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	useEffect(() => {
-		const userName = localStorage.getItem("userName");
-		setUsername(userName);
-	}, [username]);
+useEffect(() => {
+	const userName = localStorage.getItem("userName");
+	setUsername(userName);
+	setIsLoggedIn(!!userName); // Update isLoggedIn state immediately
+}, []);
+
 	return (
 		<Navbar bg="dark" variant="dark" expand="lg">
 			<Navbar.Brand href="/">GetFit</Navbar.Brand>
 			<Navbar.Toggle aria-controls="navbar-nav" />
 			<Navbar.Collapse id="navbar-nav">
 				<Nav className="ml-auto">
-					{!username && (
+					{!isLoggedIn && (
 						<>
 							<Nav.Link href="/login">Login</Nav.Link>
 							<Nav.Link href="/register">Register</Nav.Link>
@@ -25,7 +28,7 @@ const AppNavbar = () => {
 					<Nav.Link href="/challenge">Challenges</Nav.Link>
 					<Nav.Link href="/activity-today">Activity</Nav.Link>
 					<Nav.Link href="/challenge/subscribed">My Challenges</Nav.Link>
-					{username && (
+					{isLoggedIn && (
 						<>
 							<Nav.Link href="/logout">Logout</Nav.Link>
 						</>
